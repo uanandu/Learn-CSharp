e93e5a19101bb11222623fea8baf9b59a4dfcee1
uanandu <ananduun@icloud.com>
2023-09-08 13:46:11 -0400

    initial commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Here we created an object 'dice'
+Random dice = new Random();
+
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total = roll1 + roll2 + roll3;
+
+// Double bonus: If two rolls are the same we get 2 bonus points added to the total
+// Triple bonus: If all three rolls are the same we get 6 bonus points added to the total
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+    if ((roll1 == roll2) && (roll2 == roll3)){
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else {
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+    }
+}
+
+if (total >= 15){
+    if (total >= 16){
+        Console.WriteLine("You win a new car!!!!");
+    } 
+    else {
+        Console.WriteLine("You win!!");
+    }
+} else {
+    if (total >= 10){
+        Console.WriteLine("You win a laptop!");
+    } 
+    else if (total == 7){
+        Console.WriteLine("You win a trip to Gulf!");
+    }
+    else {
+        Console.WriteLine("You a kitten!!");
+    }
+} 
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
