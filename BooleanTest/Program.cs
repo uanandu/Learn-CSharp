68ab514efcf59664d580fcdc6cac75f6294dd920
uanandu <ananduun@icloud.com>
2023-09-10 14:20:14 -0400

    Boolean test added

---
+++ BooleanTest/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//"Admin|Manager";
+Console.WriteLine("Please enter your permission");
+string permission = Console.ReadLine();
+
+// Level
+Console.WriteLine("Please enter your level");
+int level = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"You are {permission} with a level {level} clearence!");
+
+if (permission.Contains("Admin") || permission.Contains("Manager")){
+    if (permission.Contains("Admin")){
+        if (level > 55){
+            Console.WriteLine("Welcome, Super Admin user.");
+        } else {
+            Console.WriteLine("Welcome, Admin user.");
+        }
+    }
+    else if (permission.Contains("Manager")){
+        if (level >= 20){
+            Console.WriteLine("Contact an Admin for access.");
+        } else {
+            Console.WriteLine("You do not have sufficient privileges.");
+        }
+    }
+} 
+else {
+    Console.WriteLine("You do not have sufficient privileges.");
+}
